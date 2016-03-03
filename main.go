a863f6c73bb42eac2b548229d14bdfa0d7153363
Curtis <curtis99877@gmail.com>
2016-03-02 23:02:13 -0500

    Config file initiation

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ type Configuration struct {
   Username string `json:"username"`
   Password string `json:"password"`
   Subreddits []string `json:"subreddits"`
-  Output string `json:"output"`
+  OutputPath string `json:"outputPath"`
   Stats bool `json:"stats"`
 }
 
@@ -25,8 +25,8 @@ func main() {
   fmt.Printf("Scraper v0.1\n")
   fmt.Printf("Created by Curtis Li\n")
 
-  // Read configuration file
-  config := readConfig(configFile)
+  // Get configuration settings
+  config := configSettings(configFile)
   fmt.Printf("%v\n", config)
 
   // Loop through subreddit list
@@ -44,15 +44,45 @@ func main() {
 }
 
 
-func readConfig(filename string) Configuration {
+func configSettings(filename string) Configuration {
   // Open config file
+  configuration := Configuration{}
   file, err := os.Open(filename)
+
+  // File does not exist, create it
+  if os.IsNotExist(err) {
+    // Create file
+    fmt.Println("Initiating new configuration file...")
+    file, err = os.Create(filename)
+    util.Check(err)
+
+    // Setup and encode the JSON
+    var b []byte
+    configuration.Username = "Username"
+    configuration.Subreddits = append(configuration.Subreddits, "/r/subreddit1", "/r/subreddit2", "/r/subreddit3")
+    configuration.OutputPath = "Path/To/Output/Folder"
+    b, err = json.MarshalIndent(configuration, "", "    ")
+    util.Check(err)
+
+    // Write to the new file
+    _, err = file.Write(b)
+    util.Check(err)
+
+    // Close the fd
+    err = file.Close()
+    util.Check(err)
+
+    // Exit
+    fmt.Println("Please edit 'config.json'")
+    os.Exit(0)
+  }
+
   util.Check(err)
 
   // Parse JSON
-  configuration := Configuration{}
   err = json.NewDecoder(file).Decode(&configuration)
   util.Check(err)
 
   return configuration
 }
+
