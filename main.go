cf36850b6255e2d8d447fa362c222d7ec7455f0e
Curtis <curtis99877@gmail.com>
2016-03-02 22:20:00 -0500

    Config file support and logging

--- main.go
+++ main.go
@@ -2,25 +2,57 @@ package main
 
 import (
   "fmt"
+  "os"
+  "encoding/json"
+  "reddit-scraper/util"
   "reddit-scraper/http"
   "reddit-scraper/reddit"
 )
 
+
+const configFile = "conf.json"
+
+type Configuration struct {
+  Username string `json:"username"`
+  Password string `json:"password"`
+  Subreddits []string `json:"subreddits"`
+  Output string `json:"output"`
+  Stats bool `json:"stats"`
+}
 
 
 func main() {
   fmt.Printf("Scraper v0.1\n")
   fmt.Printf("Created by Curtis Li\n")
 
-  subreddits := []string {"/r/funny", "/r/AskReddit", "/r/gfycats"}
+  // Read configuration file
+  config := readConfig(configFile)
+  fmt.Printf("%v\n", config)
 
-  for _, subreddit := range subreddits {
+  // Loop through subreddit list
+  for _, subreddit := range config.Subreddits {
     fmt.Println(subreddit)
+
+    // Get subreddit JSON
     listing := reddit.ListingJson{}
     http.GetJson("http://www.reddit.com/" + subreddit + "/.json", &listing)
-    fmt.Printf("%v\n", listing)
+
+    // Get downloads
     downloadUrls := reddit.DownloadPosts(listing.Data.Children)
     fmt.Println(downloadUrls)
   }
 }
 
+
+func readConfig(filename string) Configuration {
+  // Open config file
+  file, err := os.Open(filename)
+  util.Check(err)
+
+  // Parse JSON
+  configuration := Configuration{}
+  err = json.NewDecoder(file).Decode(&configuration)
+  util.Check(err)
+
+  return configuration
+}
